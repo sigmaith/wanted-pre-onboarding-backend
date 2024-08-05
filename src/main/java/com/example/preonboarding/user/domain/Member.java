7579d19c476373c825bc9158857ffdf32f1dc923
Smith <125195487+sigmaith@users.noreply.github.com>
2024-08-05 10:32:08 +0900

    feat: Member 엔티티 정의
    * README 수정
    postres의 user 예약어로 인해 "User" 명칭 사용불가. 대신 "Member" 로 변경

---
+++ src/main/java/com/example/preonboarding/user/domain/Member.java
@@ -0,0 +1,20 @@
+package com.example.preonboarding.user.domain;
+
+import com.example.preonboarding.job_application.domain.JobApplication;
+import jakarta.persistence.*;
+import java.util.List;
+
+@Entity
+public class Member {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "member_id")
+    private Long id;
+
+    private String name;
+    private String email;
+    private String password;
+
+    @OneToMany(mappedBy = "member")
+    private List<JobApplication> jobApplications;
+}
