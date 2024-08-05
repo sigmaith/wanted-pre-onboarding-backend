91d21b857e928a4c414b06342134783fbd131401
Smith <125195487+sigmaith@users.noreply.github.com>
2024-08-05 21:58:44 +0900

    refactor: JobPostingDetailsResponseDto에 JSON 필드명 지정
    - JobPostingDetailsResponseDto.java: 각 필드에 @JsonProperty 추가하여 JSON 필드명 지정

--- src/main/java/com/example/preonboarding/job_posting/dto/JobPostingDetailsResponseDto.java
+++ src/main/java/com/example/preonboarding/job_posting/dto/JobPostingDetailsResponseDto.java
@@ -1,5 +1,6 @@
 package com.example.preonboarding.job_posting.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
@@ -9,14 +10,31 @@ import java.util.List;
 @Getter
 @Setter
 public class JobPostingDetailsResponseDto {
+    @JsonProperty("채용공고_id")
     private Long id;
+
+    @JsonProperty("회사명")
     private String companyName;
+
+    @JsonProperty("국가")
     private String country;
+
+    @JsonProperty("지역")
     private String region;
+
+    @JsonProperty("채용포지션")
     private String position;
+
+    @JsonProperty("채용보상금")
     private int compensation;
+
+    @JsonProperty("사용기술")
     private String techStack;
+
+    @JsonProperty("채용내용")
     private String content;
+
+    @JsonProperty("회사가올린다른채용공고")
     private List<Long> otherJobPostings;
 
     @Builder
