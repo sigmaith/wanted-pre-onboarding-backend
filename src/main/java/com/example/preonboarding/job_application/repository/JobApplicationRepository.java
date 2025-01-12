20f05b74768f6678928b90333c8252fbd3690804
Sumi Chung <drsoombre@naver.com>
2025-01-12 10:23:18 +0900

    채용 공고 및 멤버아이디로 지원내역 조회 쿼리 수정

--- src/main/java/com/example/preonboarding/job_application/repository/JobApplicationRepository.java
+++ src/main/java/com/example/preonboarding/job_application/repository/JobApplicationRepository.java
@@ -5,7 +5,12 @@ import com.example.preonboarding.job_application.domain.JobApplication;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface JobApplicationRepository extends JpaRepository<JobApplication, Long> {
-    Optional<JobApplication> findByJobPostingAndMember(Long jobPostingId, Long memberId);
+//    Optional<JobApplication> findByJobPostingAndMember(Long jobPostingId, Long memberId);
+    @Query("SELECT ja FROM JobApplication ja WHERE ja.jobPosting.id = :jobPostingId AND ja.member.id = :memberId")
+    Optional<JobApplication> findByJobPostingAndMember(@Param("jobPostingId") Long jobPostingId, @Param("memberId") Long memberId);
+
 }
